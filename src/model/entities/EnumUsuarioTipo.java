7c42191d91abf42ea80fd52bc8ce6aa55135a2ee
Lucas Bernadino <luucasdinoo@gmail.com>
2024-04-25 18:51:31 -0300

    entidade-usuario

---
+++ src/model/entities/EnumUsuarioTipo.java
@@ -0,0 +1,9 @@
+package model.entities;
+
+public enum EnumUsuarioTipo {
+
+    RIBEIRINHO,
+    COOPERATIVA,
+    RESTAURANTE;
+
+}
---
+++ src/model/entities/Usuario.java
@@ -0,0 +1,70 @@
+package model.entities;
+
+public class Usuario {
+
+    private String nome;
+    private String email;
+    private String cpfOuCnpj;
+    private String senha;
+    private String contato;
+    private EnderecoUsuario endereco;
+    private EnumUsuarioTipo tipo;
+
+    public Usuario(String nome, String email, String cpfOuCnpj, String senha, String contato, EnderecoUsuario endereco, EnumUsuarioTipo tipo) {
+        this.nome = nome;
+        this.email = email;
+        this.cpfOuCnpj = cpfOuCnpj;
+        this.senha = senha;
+        this.contato = contato;
+        this.endereco = endereco;
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpfOuCnpj() {
+        return cpfOuCnpj;
+    }
+
+    public void setCpfOuCnpj(String cpfOuCnpj) {
+        this.cpfOuCnpj = cpfOuCnpj;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public EnderecoUsuario getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(EnderecoUsuario endereco) {
+        this.endereco = endereco;
+    }
+
+    public EnumUsuarioTipo getTipo() {
+        return tipo;
+    }
+}
