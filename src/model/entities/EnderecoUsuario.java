152c80f2cd7fabf0a0c22ac252f78f090490e188
Lucas Bernadino <luucasdinoo@gmail.com>
2024-04-25 18:53:55 -0300

    add-endereco

---
+++ src/model/entities/EnderecoUsuario.java
@@ -0,0 +1,38 @@
+package model.entities;
+
+public class EnderecoUsuario {
+
+    private String nomeEndereco;
+    private Integer numeroEndereco;
+    private String complementoEndereco;
+
+    public EnderecoUsuario(String nomeEndereco, Integer numeroEndereco, String complementoEndereco) {
+        this.nomeEndereco = nomeEndereco;
+        this.numeroEndereco = numeroEndereco;
+        this.complementoEndereco = complementoEndereco;
+    }
+
+    public String getNomeEndereco() {
+        return nomeEndereco;
+    }
+
+    public void setNomeEndereco(String nomeEndereco) {
+        this.nomeEndereco = nomeEndereco;
+    }
+
+    public Integer getNumeroEndereco() {
+        return numeroEndereco;
+    }
+
+    public void setNumeroEndereco(Integer numeroEndereco) {
+        this.numeroEndereco = numeroEndereco;
+    }
+
+    public String getComplementoEndereco() {
+        return complementoEndereco;
+    }
+
+    public void setComplementoEndereco(String complementoEndereco) {
+        this.complementoEndereco = complementoEndereco;
+    }
+}
